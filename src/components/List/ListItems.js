fb77ed687d1ff9b032d0fa36e76c438c08c8492c
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-04-26 09:23:18 +0200

    initial commit

---
+++ src/components/List/ListItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ListItems.css";
+
+function ListItems() {
+  return (
+    <div className="listitem-container">
+      <div className="listitem">List Item 1</div>
+      <div className="listitem">List Item 2</div>
+      <div className="listitem">List Item 3</div>
+      <div className="listitem">List Item 4</div>
+      <div className="listitem">List Item 5</div>
+      <div className="listitem">List Item 6</div>
+    </div>
+  );
+}
+
+export default ListItems;
